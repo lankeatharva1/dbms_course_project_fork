08e4acd56a6e71ed4fd6ee45313f9ba7f4d50a61
lankeatharva1 <lankeatharva1@gmail.com>
2026-04-26 20:32:03 +0530

    fix: use absolute path for static files on Render

--- server_unified.js
+++ server_unified.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
@@ -18,7 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, '.')));
 app.set('trust proxy', 1);
 app.use(session({
   secret: process.env.SESSION_SECRET,
